6744dedb6ca9360a09182a10808631a6c840c0cd
charlie-too <cfiel012@ucr.edu>
2025-11-24 16:33:01 -0800

    Add files via upload

---
+++ 00_requirements.R
@@ -0,0 +1,18 @@
+
+install.packages("fredr", dependencies = TRUE)
+installed.packages()["fredr",]
+install.packages("fredr")
+library(fredr)
+##We install this package for knowing the US recession.
+
+library(dplyr)
+##Use it to code for data flames.
+
+install.packages("ggplot2")
+library("ggplot2")
+##use for visual data.
+
+install.packages("quantmod")
+library(quantmod)
+##This data is use for answer the Market stock and trade value.
+
